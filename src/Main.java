6fc05acc3bf5aac89a911075fc701a6d454dedbc
Tyson <tcraner@unm.edu>
2017-10-17 18:21:37 -0600

    Testing MapFromXML class

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,18 @@
-
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import util.MapFromXML;
 
-public class Main extends Application {
-
+public class Main extends Application
+{
     private Group root = new Group();
     private GridPane pane = new GridPane();
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-
+    public void start(Stage primaryStage) throws Exception
+    {
         Station test = new Station("A", 0);
         Track track = new Track(100, 100, false);
         pane.getChildren().addAll(test, track);
@@ -24,12 +20,15 @@ public class Main extends Application {
         root.getChildren().add(pane);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1000, 1000));
-
         primaryStage.show();
+
+        //Test new XML reader class
+        new MapFromXML();
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 }
