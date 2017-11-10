eea3c94c22cb58f9aec17c78798ecf72a7509f6f
alexGonzales <31605796+alexGonzales@users.noreply.github.com>
2017-11-09 17:36:15 -0700

    Test

--- src/DisplayGUI.java
+++ src/DisplayGUI.java
@@ -105,9 +105,6 @@ public class DisplayGUI extends AnimationTimer {
                             }
                         }
                     }
-
-
-
                 }
             }
         }
